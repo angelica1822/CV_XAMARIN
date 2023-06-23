0c35b7836635841c6e90c52df943c603d94cbec0
angelica1822 <113204229+angelica1822@users.noreply.github.com>
2023-06-23 16:58:00 -0500

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
 
 namespace CVApp
 {
@@ -20,8 +22,36 @@ namespace CVApp
             var experienciaLaboral = ((Editor)fieldsLayout.Children[11]).Text;
             var formacion = ((Editor)fieldsLayout.Children[13]).Text;
 
+            // Crear el documento PDF
+            var document = new PdfDocument();
+            var page = document.AddPage();
 
-            DisplayAlert("Guardado", "Los datos han sido guardados correctamente.", "OK");
+            using (var gfx = XGraphics.FromPdfPage(page))
+            {
+                // Agregar los datos al documento
+                var font = new XFont("Arial", 12);
+                var yPosition = 50;
+
+                gfx.DrawString("Contacto:", font, XBrushes.Black, new XPoint(50, yPosition));
+                gfx.DrawString(contacto, font, XBrushes.Black, new XPoint(150, yPosition));
+                yPosition += 20;
+
+                gfx.DrawString("Idiomas:", font, XBrushes.Black, new XPoint(50, yPosition));
+                gfx.DrawString(idiomas, font, XBrushes.Black, new XPoint(150, yPosition));
+                yPosition += 20;
+
+                gfx.DrawString("Habilidades:", font, XBrushes.Black, new XPoint(50, yPosition));
+                gfx.DrawString(habilidades, font, XBrushes.Black, new XPoint(150, yPosition));
+                yPosition += 20;
+
+                // Continuar agregando los demás campos del CV según tu diseño
+
+                // Guardar el documento en un archivo
+                var filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CV.pdf");
+                document.Save(filePath);
+            }
+
+            DisplayAlert("Guardado", "El CV se ha generado correctamente.", "OK");
         }
     }
 }
